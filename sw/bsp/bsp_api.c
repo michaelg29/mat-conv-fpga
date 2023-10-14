2c1cecde5286980ace8e7b064cdbeaf0a0c196ea
FredMartin123 <90061879+FredMartin123@users.noreply.github.com>
2023-10-14 16:28:17 -0400

    Pseudo code done

--- sw/bsp/bsp_api.c
+++ sw/bsp/bsp_api.c
@@ -3,11 +3,48 @@
  *
  *  Created on: September 28, 2023
  *      Author: Jacoby
+ * 
+ *    Modified: October 13, 2023
+ *      Author: Frederik Martin
  */
 
 #include "bsp_api.h"
 
+#define S_KEY_DEFAULT 0xCAFECAFE
+#define E_KEY_DEFAULT 0xDEADBEEF
 
+void dummy() {}
+rx_callback_t callback = &dummy;
+uint32_t trans_id = 0xFFFFFFFF;
+
+
+
+/**
+ * @brief Gets the next transaction ID.
+ * @note Increments the current trans_id and prevents it from overflowing to 0.
+ *       
+ * 
+ * @retval Next trans_id.
+*/
+uint32_t getNextTransID(){
+  trans_id +=2; //by starting with an odd value and always incrementing by 2, the value will never equal zero even when overflowing
+  return trans_id;
+}
+
+/**
+ * @brief Calculates the checksum of a header.    
+ * 
+ * @retval Checksum of the header.
+*/
+uint32_t calculateChecksum(header_t h){
+  return  h.S_KEY +
+          h.COMMAND +
+          h.SIZE +
+          h.TX_ADDR + 
+          h.TRANS_ID +
+          h.STATUS + 
+          h.COMMAND;
+}
 
 
 
@@ -21,6 +58,8 @@
   * @retval None
   */
 void moduleConfig(apb_ctrl_registers_t regs){
+
+  (*MODULE_APB_ADDR_REGSTATUS) = regs;
 }
 
 
@@ -34,6 +73,10 @@ void moduleConfig(apb_ctrl_registers_t regs){
   *	    value of the status registers.
   */
 apb_state_registers_t moduleGetState(void){
+
+  apb_state_registers_t state_regs;
+  memcpy(&state_regs, MODULE_APB_ADDR_STATUS, sizeof(apb_state_registers_t));
+  return state_regs;
 }
 
 
@@ -45,11 +88,93 @@ apb_state_registers_t moduleGetState(void){
   * @param  addr  	Address of the kernel to be sent
   * @param  dimension  	The dimension (in pixels) of the row or column of the kernel sent 
   *			(assumed to be a square matrix).
-  * @param  ordering	Specifies if the image is row major (=0) or column major (=1).
   *  
-  * @retval The function returns an error code (if 0 -> no error. if <0 -> error). TODO specify more error codes
+  * @retval The function returns an error code (if 0 -> no error. if > 0 -> error). TODO specify more error codes
   */
-int8_t sendKernel(uint32_t addr, uint32_t dimension, uint8_t ordering){
+int32_t sendKernel(uint32_t addr, uint32_t dimension){
+
+  header_t header, ACK;
+  header.S_KEY = S_KEY_DEFAULT;
+  header.COMMAND = LOAD_KERNEL;
+  header.SIZE = (dimension<<16) | (0xFFFF & dimension);
+  header.TX_ADDR = &ACK;
+  header.TRANS_ID = getNextTransID();
+  header.STATUS = 0; //reserved
+  header.E_KEY = E_KEY_DEFAULT;
+  header.CHKSUM = calculateChecksum(header);
+
+  //writeTO(MODULE_AXI_SLV_ADDR, header, sizeof(header));
+  //writeTO(MODULE_AXI_SLV_ADDR, addr, dimension*dimension);
+
+  //todo more robust error checks
+  uint8_t error;
+  if(ACK.CHKSUM != calculateChecksum(ACK)){
+    error = ERR_CHECKSUM;
+  }
+  else {
+    error = ACK.STATUS;
+  }
+
+  return error;
+
+}
+
+/**
+  * @brief  Send a kernel to the module using DMA. Non-blocking version of "sendKernel".
+  * @note   This function assumes that the module has been initialized with "moduleConfig". This function abstracts
+  * 	    the header information for the transaction.
+  *
+  * @param  addr  	Address of the kernel to be sent
+  * @param  dimension  	The dimension (in pixels) of the row or column of the kernel sent 
+  *			(assumed to be a square matrix).
+  *  
+  * @retval The function returns an error code (if 0 -> no error. if > 0 -> error). TODO specify more error codes
+  */
+uint32_t sendKernelAsync(uint32_t addr, uint32_t dimension) {
+
+  header_t header, ACK;
+  header.S_KEY = S_KEY_DEFAULT;
+  header.COMMAND = LOAD_KERNEL;
+  header.SIZE = (dimension<<16) | (0xFFFF & dimension);
+  header.TX_ADDR = &ACK;
+  header.TRANS_ID = getNextTransID();
+  header.STATUS = 0; //reserved
+  header.E_KEY = E_KEY_DEFAULT;
+  header.CHKSUM = calculateChecksum(header);
+
+  //writeTO(MODULE_AXI_SLV_ADDR, header, sizeof(header));
+
+  //DMA_TCDn_SADDR = addr;
+
+  //DMA_TCDn_ATTR |= (0b011) << 8 || 0b011; 64 bit transfers
+	
+  //DMA_TCDn_SOFF |= 8; //64 bit transfers, 8 bytes
+
+  //DMA_TCDn_SLAST = -dimension*dimension;
+
+  //DMA_TCDn_DADDR = MODULE_AXI_SLV_ADDR;
+
+  //DMA_TCDn_NBYTES = dimension*dimension;
+
+  //DMA_TCDn_CITER = 1;
+  //DMA_TCDn_BITER = 1;
+
+  //DMA_TCDn_DOFF = 0;
+
+  //DMA_TCDn_CSR |= (0b11<<14);
+
+
+  //todo more robust error checks
+  uint32_t error;
+  if(ACK.CHKSUM != calculateChecksum(ACK)){
+    error = ERR_CHECKSUM;
+  }
+  else {
+    error = ACK.STATUS;
+  }
+
+  return error;
+
 }
 
 
@@ -60,12 +185,95 @@ int8_t sendKernel(uint32_t addr, uint32_t dimension, uint8_t ordering){
   *
   * @param  addr  	Address of the image to be sent
   * @param  size  	The size (in pixels) of the image sent
-  * @param  ordering	Specifies if the image is row major (=0) or column major (=1).
   *  
-  * @retval The function returns an error code (if 0 -> no error. if <0 -> error). TODO specify more error codes
+  * @retval The function returns an error code (if 0 -> no error. if > 0 -> error). TODO specify more error codes
   */
-int8_t  sendImage(uint32_t addr, uint32_t size, uint8_t ordering){
+uint32_t sendImage(uint32_t addr, uint32_t rows, uint32_t cols, uint32_t outAddr) {
+
+  header_t header, ACK;
+  header.S_KEY = S_KEY_DEFAULT;
+  header.COMMAND = (LOAD_MATRIX << 30) | (0x3fffffff & outAddr);
+  header.SIZE = (rows<<16) | (0xFFFF & cols);
+  header.TX_ADDR = &ACK;
+  header.TRANS_ID = getNextTransID();
+  header.STATUS = 0; //reserved
+  header.E_KEY = E_KEY_DEFAULT;
+  header.CHKSUM = calculateChecksum(header);
+
+  //writeTO(MODULE_AXI_SLV_ADDR, header, sizeof(header));
+  //writeTO(MODULE_AXI_SLV_ADDR, addr, rows*cols);
+
+  //todo more robust error checks
+  uint32_t error;
+  if(ACK.CHKSUM != calculateChecksum(ACK)){
+    error = ERR_CHECKSUM;
+  }
+  else {
+    error = ACK.STATUS;
+  }
+
+  return error;  
 }
+
+
+/**
+  * @brief  Send an image to the module using DMA. Non-blocking version of "sendImage".
+  * @note   This function assumes that the module has been initialized with "moduleConfig". This function abstracts
+  * 	    the header information for the transaction.
+  *
+  * @param  addr  	Address of the image to be sent
+  * @param  size  	The size (in pixels) of the image sent
+  *  
+  * @retval The function returns an error code (if 0 -> no error. if > 0 -> error). TODO specify more error codes
+  */
+uint32_t sendImageAsync(uint32_t addr, uint32_t rows, uint32_t cols, uint32_t outAddr){
+
+
+  header_t header, ACK;
+  header.S_KEY = S_KEY_DEFAULT;
+  header.COMMAND = (LOAD_MATRIX << 30) | (0x3fffffff & outAddr);
+  header.SIZE = (rows<<16) | (0xFFFF & cols);
+  header.TX_ADDR = &ACK;
+  header.TRANS_ID = getNextTransID();
+  header.STATUS = 0; //reserved
+  header.E_KEY = E_KEY_DEFAULT;
+  header.CHKSUM = calculateChecksum(header);
+
+  //writeTO(MODULE_AXI_SLV_ADDR, header, sizeof(header));
+
+  //DMA_TCDn_SADDR = addr;
+
+  //DMA_TCDn_ATTR |= (0b011) << 8 || 0b011; 64 bit transfers
+	
+  //DMA_TCDn_SOFF |= 8; //64 bit transfers, 8 bytes
+
+  //DMA_TCDn_SLAST = -size;
+
+  //DMA_TCDn_DADDR = MODULE_AXI_SLV_ADDR;
+
+  //DMA_TCDn_NBYTES = size;
+
+  //DMA_TCDn_CITER = 1;
+  //DMA_TCDn_BITER = 1;
+
+  //DMA_TCDn_DOFF = 0;
+
+  //DMA_TCDn_CSR |= (0b11<<14);
+
+
+  //todo more robust error checks
+  uint32_t error;
+  if(ACK.CHKSUM != calculateChecksum(ACK)){
+    error = ERR_CHECKSUM;
+  }
+  else {
+    error = ACK.STATUS;
+  }
+
+  return error;
+}
+
+
 
 
 /**
@@ -78,6 +286,9 @@ int8_t  sendImage(uint32_t addr, uint32_t size, uint8_t ordering){
   * @retval None
   */
 int8_t sendCommand(commands_e com){
+
+
+
 }
 
 
@@ -92,7 +303,25 @@ int8_t sendCommand(commands_e com){
   * @retval The function returns the previous callback that was assigned.
   */
 rx_callback_t registerCallback(rx_callback_t cb){
+
+   rx_callback_t oldCB = callback;
+  
+   if(cb == NULL){
+     callback = &dummy;
+   }
+   else{
+      callback = cb;
+   }
+   
+   if(oldCB == &dummy){
+      return NULL;
+   }
+   else{
+      return oldCB;
+   }
+   
 }
+
 
 
 
--- sw/bsp/bsp_api.h
+++ sw/bsp/bsp_api.h
@@ -3,7 +3,9 @@
  *
  *  Created on: September 28, 2023
  *      Author: Jacoby
- *
+ * 
+ *  Modified: October 13, 2023
+ *      Author: Frederik Martin
  * 
  *  Description:
  *  This is the BSP API to interface with the FPGA convolution module.
@@ -40,8 +42,22 @@ extern "C" {
 typedef void (*rx_callback_t)(void);
 
 typedef enum{
-	//TODO list commands
+	LOAD_KERNEL = 0,
+	LOAD_MATRIX = 1
 }commands_e;
+
+typedef enum{
+	OK 					= 0x0,
+	ERR 				= 0x1,
+	ERR_SIZE_INVALID 	= 0x2,
+	ERR_NO_KERNEL 		= 0x4,
+	//0x8,
+	//0x10,
+	//0x20,
+	//0x40,
+	//0x80,
+	ERR_CHECKSUM = 0x80000000;
+} status_e;
 
 typedef struct{
 
@@ -64,12 +80,27 @@ typedef struct{
 
 } module_t; 
 
+typedef struct{
+
+	uint32_t S_KEY; //Header start key 
+	uint32_t COMMAND; //Command to execute  
+	uint32_t SIZE; //Size of the input matrix 
+	uint32_t TX_ADDR; //Address to send the response packet  
+	uint32_t TRANS_ID; //Transaction ID 
+	uint32_t STATUS; //STATUS
+	uint32_t E_KEY ; //Header end key  
+	uint32_t CHKSUM; //Header checksum 
+
+
+} header_t; 
 
 
 void moduleConfig(apb_ctrl_registers_t regs);
 apb_state_registers_t moduleGetState(void);
-int8_t sendKernel(uint32_t addr, uint32_t dimension);
-int8_t sendImage(uint32_t addr, uint32_t size, uint8_t ordering);
+uint32_t sendKernel(uint32_t addr, uint32_t dimension);
+uint32_t sendKernelAsync(uint32_t addr, uint32_t dimension);
+uint32_t sendImage(uint32_t addr, uint32_t size);
+uint32_t sendImageAsync(uint32_t addr, uint32_t rows, uint32_t cols, uint32_t outAddr);
 int8_t sendCommand(commands_e com);
 rx_callback_t registerCallback(rx_callback_t cb);
 
