923e6605081faafe4ca84b6cb6ca4667f8492e7f
Jacoby Roy <jacobyr2010@hotmail.ca>
2024-02-07 13:47:39 -0500

    Core testbench done and tested

--- rtl/hdl/tb_core/tb_top.sv
+++ rtl/hdl/tb_core/tb_top.sv
@@ -5,10 +5,13 @@
 module tb_top
     #(
         parameter KERNEL_SIZE = 5,
-        parameter MAX_PIXEL_VAL = 40'hFF,
-        parameter MAX_KERNEL_VAL = 40'hFF,
-        parameter MAX_SUB_VAL = 18'h8,
-        parameter PIPELINE = 0,
+        parameter MIN_PIXEL_VAL = 40'h160,
+        parameter MAX_PIXEL_VAL = 40'h210,
+        parameter MIN_KERNEL_VAL = 40'h160,
+        parameter MAX_KERNEL_VAL = 40'h210,
+        parameter MIN_SUB_VAL = 18'h0,
+        parameter MAX_SUB_VAL = 18'h10,
+        parameter PIPELINE = 1,
         parameter ROUNDING = 3'b100
     );
 
@@ -17,26 +20,24 @@ module tb_top
     // Signals
     reg i_clk;
     reg i_en;
-    reg i_rst_n;
     logic [KERNEL_SIZE-1:0][7:0] i_pixels;
     logic [KERNEL_SIZE-1:0][7:0] i_kernels;
     logic [17:0] i_sub;
     wire [17:0] o_res;
-    wire o_valid; //TO REMOVE
 
     // Variables declarations (packed arrays)
-    var reg [4:0][7:0] ireg1 = 0; // 5 pixels
-    var reg [4:0][7:0] jreg1 = 0; // 5 kernel values
-    var reg [17:0] kreg1 = 0; // sub
-    var reg [4:0][7:0] ireg2 = 0; // 5 pixels
-    var reg [4:0][7:0] jreg2 = 0; // 5 kernel values
-    var reg [17:0] kreg2 = 0; // sub
+    var longint i1 = 0; // 5 pixels
+    var longint j1 = 0; // 5 kernel values
+    var longint k1 = 0; // sub
+    var longint i2 = 0; // 5 pixels
+    var longint j2 = 0; // 5 kernel values
+    var longint k2 = 0; // sub
     var longint oreg = 0;
 
-    // use time to get 64-bit unsigned int
-    var time i;
-    var time j;
-    var time k;
+    // use time to get 64-bit signed int (only need 40-bits for i and j)
+    var longint i;
+    var longint j;
+    var longint k;
 
     // Clock
     const int clk_period = 200; //ns (5MHz)
@@ -53,7 +54,6 @@ module tb_top
     core DUT(
         .i_clk      (i_clk),
         .i_en       (i_en),
-        .i_rst_n    (i_rst_n),
         .i_s0       (i_pixels[0]),
         .i_s1       (i_pixels[1]),
         .i_s2       (i_pixels[2]),
@@ -65,8 +65,7 @@ module tb_top
         .i_k3       (i_kernels[3]),
         .i_k4       (i_kernels[4]),
         .i_sub      (i_sub),
-        .o_res      (o_res),
-        .o_valid    (o_valid) //REMOVE IN CORE DESIGN
+        .o_res      (o_res)
     );
 
     // Testbench
@@ -78,34 +77,34 @@ module tb_top
     initial begin
 
         $display("Core Testbench Starts");
+        if(PIPELINE == 1) begin
+            $display("Pipelined testbench");
+        end else begin
+            $display("Non-pipelined testbench");
+        end
 
         // Reset all signals to 0
         i_en = 0;
-        i_rst_n = 0;
         i_pixels = 0;
         i_kernels = 0;
         i_sub = 0;
 
         // Enable the core
-        @(posedge i_clk); 
+        @(negedge i_clk);
         i_en = 1;
 
-        //Put core out of reset
-        @(posedge i_clk);
-        i_rst_n = 1;
-
-        @(posedge i_clk);
+        @(negedge i_clk);
             
         // Pixel value iteration
         // (iterate through all possible pixels combinations)
-        for(i = 0 ; i <= MAX_PIXEL_VAL ; i++) begin
+        for(i = MIN_PIXEL_VAL ; i <= MAX_PIXEL_VAL ; i++) begin
 
             // Kernel value iteration
             // (iterate through all possible kernel values combinations)
-            for(j = 0 ; j <= MAX_KERNEL_VAL ; j++) begin
+            for(j = MIN_KERNEL_VAL ; j <= MAX_KERNEL_VAL ; j++) begin
                 
                 // Sub-result value iteration
-                for(k = 0 ; k <= MAX_SUB_VAL ; k++) begin
+                for(k = MIN_SUB_VAL ; k <= MAX_SUB_VAL ; k++) begin
 
                     // once a input is given, it takes 2 clock cycles
                     // before output. 
@@ -113,49 +112,49 @@ module tb_top
 
                     if(PIPELINE == 1) begin
 
-                        //TODO FIX MATH BLOCK FIRST
-
                         // For each new values:
                         // First clock cycle : assign new values (pipeline)
-                        // Second clock cycle: check output values
-                        @(negedge i_clk);
+                        // Second clock cycle : wait for computation
+                        // Third clock cycle: check output values
                         
-                        // New values
+                        // New values (first clk cycle)
                         i_pixels = i;  
                         i_kernels = j; 
                         i_sub = k;
 
+                        // Load input/Get output result
+                        @(posedge i_clk); 
 
-                        // Calculate value that should be obtained
-                        oreg = kreg1;
+                        @(negedge i_clk); //o_res has correct value
+
+                        // Calculate value that should be obtained (thid clock cycle check)
+                        oreg = k1;
                         oreg += ROUNDING;
                         for (int s = 0 ; s < KERNEL_SIZE ; s++) begin
-                            oreg += ireg1[s] * kreg1[s];
+                            // Use variable part-select with fixed width
+                            oreg += signed'(i1[8*s +: 8]) * signed'(j1[8*s +: 8]);
                         end
 
-                        // Wait for output result
-                        @(posedge i_clk);
-
-                        $display("oreg: %d ; o_res: %d",oreg[20:3], o_res);
-
                         // if not first or second input
-                        if((i != 0) || (j != 0) || ((k != 0) || (k != 1))) begin
+                        if((i != 0) || (j != 0) || ((k != 0) && (k != 1))) begin
                             // Compare output to valid result
                             if(oreg[20:3] != o_res) begin
+                                @(negedge i_clk);
                                 $display("Test failed for i: %d ; j: %d ; k: %d",i,j,k);
+                                $display("oreg[20:3]: %d ; oreg: %d ; o_res: %d",oreg[20:3], oreg, o_res);
                                 $finish(2);
                             end
                         end
 
-                        // Get input from previous
-                        ireg1 = ireg2;
-                        jreg1 = jreg2;
-                        kreg1 = kreg2;
+                        // Get input from previous (ready for third clock cycle)
+                        i1 = i2;
+                        j1 = j2;
+                        k1 = k2;
 
-                        // Save current input for next check
-                        ireg2 = i;
-                        jreg2 = j;
-                        kreg2 = k;
+                        // Save current input for next check (ready for second clock cycle)
+                        i2 = i;
+                        j2 = j;
+                        k2 = k;
                     end else begin
                         // For each new values:
                         // First clock cycle : assign new values
@@ -169,12 +168,18 @@ module tb_top
                         i_sub = k;
 
                         // Wait for output result
-                        @(posedge i_clk); //input in
-                        @(posedge i_clk); //wait
-                        @(posedge i_clk); //wait
-                        @(posedge i_clk); //output ready
+                        @(posedge i_clk);
 
                         @(negedge i_clk);
+                        // Reset values
+                        i_pixels = 0;  
+                        i_kernels = 0; 
+                        i_sub = 0;
+
+                        @(posedge i_clk); //computation wait
+                        @(posedge i_clk); //output ready
+
+                        @(negedge i_clk); //o_res has correct value
 
                         // Calculate value that should be obtained
                         oreg = k;
@@ -188,7 +193,7 @@ module tb_top
                         if(oreg[20:3] != o_res) begin
                             @(negedge i_clk);
                             $display("Test failed for i: %d ; j: %d ; k: %d",i,j,k);
-                            $display("oreg[]: %d ; oreg: %d ; o_res: %d",oreg[20:3], oreg, o_res);
+                            $display("oreg[20:3]: %d ; oreg: %d ; o_res: %d",oreg[20:3], oreg, o_res);
                             $finish(2);
                         end
 
