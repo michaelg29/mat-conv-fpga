0e096b82b73030911c5de7437caa6da0302cc113
Jacoby Roy <jacobyr2010@hotmail.ca>
2024-02-16 17:56:32 -0500

    Cluster Feeder testbench completed

--- rtl/hdl/tb_cluster_feeder/tb_top.sv
+++ rtl/hdl/tb_cluster_feeder/tb_top.sv
@@ -8,9 +8,10 @@ module tb_top
         parameter FIFO_WIDTH = 8,
         parameter KERNEL_SIZE = 5,
         parameter MAX_PIXEL_VAL = 10,
-        parameter int TESTS_TO_RUN[3] = '{1,2}, // IDs of tasks to run
-        parameter int NUM_REPS = 5, //Number of times each test shall be reapeated with different values
-        parameter int SEED = 0 //Seed for the random input generation
+        parameter int TESTS_TO_RUN[] = '{1,2}, // IDs of tasks to run
+        parameter int NUM_REPS = 3, //Number of times each test shall be reapeated with different values
+        parameter int SEED = 0, //Seed for the random input generation
+        parameter int VERBOSE = 0 //Enable verbosity for debug
     );
 
     import uvm_pkg::*;
@@ -77,7 +78,7 @@ module tb_top
             // Reset IP for test
             //reset(.clk(i_clk), .en(), .rst());
 
-            $display("Running test %0d", TESTS_TO_RUN[i]);
+            `uvm_info("tb_top", $sformatf("Running test %0d", TESTS_TO_RUN[i]), UVM_NONE);
             case (TESTS_TO_RUN[i])
                 1 : begin      
                     test1(.i_clk(i_clk),
@@ -153,12 +154,15 @@ module tb_top
                 i_new = 1'b0; // pipeline shall shift
 
                 for (int i = 0 ; i < (FIFO_WIDTH-KERNEL_SIZE+1) ; i++) begin
-                    //$display("o_pixels = 0x%X ; expected = 0x%X for i: %i", o_pixels, i_pixels[i+:5], i);
 
-                    if(i != 0) begin
+                    if(i != 0) begin //pixels already shifted for first iteration
                         //Shift pixels
                         @(posedge i_clk); // 1 clock cycle to output the data
                         @(negedge i_clk); // let data appear at output
+                    end
+
+                    if(VERBOSE) begin
+                        $display("o_pixels = 0x%X ; expected = 0x%X for i: %i", o_pixels, i_pixels[i+:5], i);
                     end
 
                     // check pixels
@@ -172,7 +176,7 @@ module tb_top
                 
             end
 
-            $display("Test 1 passed");
+            `uvm_info("tb_top", "Test 1 passed", UVM_NONE);
         end
 
     endtask : test1
@@ -187,14 +191,21 @@ module tb_top
         ref reg i_new;
         ref reg i_sel;
 
+        reg [KERNEL_SIZE+FIFO_WIDTH-1:0][7:0] rx_pixels;
+
         begin
 
             /*
              * Parallel load
              */
+            if(VERBOSE) begin
+               $display("Parallel load");
+            end
+
             @(negedge i_clk);
             i_pixels = 64'hBEEF50B3CAFE6688;
             //assert(std::randomize(i_pixels)); //NEED LICENSE
+
             i_sel = 1'b1; // parallel load
             i_new = 1'b1; // pipeline shall load
             @(posedge i_clk);
@@ -202,13 +213,17 @@ module tb_top
             i_sel = 1'b0; // switch to serial load
             i_new = 1'b0; // pipeline shall shift
 
-            for (int i = 0 ; i < (FIFO_WIDTH-KERNEL_SIZE+1) ; i++) begin
-                //$display("o_pixels = 0x%X ; expected = 0x%X for i: %i", o_pixels, i_pixels[i+:5], i);
+            //Iterate until only one pixel left to load serially from pipeline
+            for (int i = 0 ; i < (FIFO_WIDTH-KERNEL_SIZE) ; i++) begin
 
-                if(i != 0) begin
+                if(i != 0) begin //pixels already shifted for first iteration
                     //Shift pixels
                     @(posedge i_clk); // 1 clock cycle to output the data
                     @(negedge i_clk); // let data appear at output
+                end
+
+                if(VERBOSE) begin
+                    $display("o_pixels = 0x%X ; expected = 0x%X for i: %i", o_pixels, i_pixels[i+:5], i);
                 end
 
                 // check pixels
@@ -224,37 +239,73 @@ module tb_top
             /*
              * Serial load
              */
+            if(VERBOSE) begin
+               $display("Serial load");
+            end
+
              for (int j = 0 ; j < NUM_REPS ; j++) begin
 
-                i_pixels = 64'hBEEF50B3CAFE6688;
+                if(j==0) begin //if previous is parallel load
+                    //Save input pixels
+                    rx_pixels[KERNEL_SIZE-1:0] = i_pixels[FIFO_WIDTH-1:FIFO_WIDTH-KERNEL_SIZE];
+                end else begin //if previous is serial load
+                    rx_pixels[KERNEL_SIZE-1:0] = i_pixels[FIFO_WIDTH-1:FIFO_WIDTH-KERNEL_SIZE];
+                end
+
+                //Select new pixels
+                if(j==0) begin
+                    i_pixels = 64'h45BEEF9CFECAC0FF;
+                end else if (j==1) begin
+                    i_pixels = 64'h0123456789101112;
+                end else if (j==2) begin
+                    i_pixels = 64'hBEEF50B3CAFE6688;
+                end else begin
+                    i_pixels = 64'h45BEEF9CFECAC0FF;
+                end
                 //assert(std::randomize(i_pixels)); //NEED LICENSE
+
+                //Save new serial pixels
+                rx_pixels[FIFO_WIDTH+KERNEL_SIZE-1:KERNEL_SIZE] = i_pixels;
+
+                if(VERBOSE) begin
+                    $display("RX pixels: 0x%X", rx_pixels);
+                end
+
                 i_sel = 1'b0; // switch to serial load
-                i_new = 1'b1; // pipeline shall load (note that the current output pixel of the pipeline shall still be shifted while the new pixels are loaded)
+                i_new = 1'b1; // pipeline shall load 
+                //NOTE: the current output pixel of the pipeline shall still be shifted while the new pixels are loaded
+
                 @(posedge i_clk);
                 @(negedge i_clk);
                 i_new = 1'b0; // pipeline shall shift
 
-                for (int i = 0 ; i < (FIFO_WIDTH-KERNEL_SIZE+1) ; i++) begin
-                    $display("o_pixels = 0x%X ; expected = 0x%X for i: %i", o_pixels, i_pixels[i+:5], i);
+                //Shift until all new pixels have been seen
+                for (int i = 0 ; i < FIFO_WIDTH ; i++) begin
 
-                    if(i != 0) begin
+                    if(i != 0) begin //pixels already shifted for first iteration
                         //Shift pixels
                         @(posedge i_clk); // 1 clock cycle to output the data
                         @(negedge i_clk); // let data appear at output
                     end
 
+                    if(VERBOSE) begin
+                        $display("o_pixels = 0x%X ; expected = 0x%X for i: %i", o_pixels, rx_pixels[i+:5], i);
+                    end
+
                     // check pixels
                     // variable part select
-                    if(i_pixels[i+:5] != o_pixels) begin
-                        `uvm_error("tb_top", $sformatf("Test 1 failed at i = %d\r\no_pixels = 0x%X ; expected = 0x%X",i,o_pixels, i_pixels[i+:5]))
+                    if(rx_pixels[i+:5] != o_pixels) begin
+                        `uvm_error("tb_top", $sformatf("Test 1 failed at i = %d\r\no_pixels = 0x%X ; expected = 0x%X",i,o_pixels, rx_pixels[i+:5]))
                         @(negedge i_clk); // let data appear at output
                         $finish(2);
                     end
                 end
 
+                //Save current output pixels
+                rx_pixels[FIFO_WIDTH-1:0] = i_pixels;
             end
 
-            $display("Task 2 passed");
+            `uvm_info("tb_top", "Test 2 passed", UVM_NONE);
         end
 
     endtask : test2
