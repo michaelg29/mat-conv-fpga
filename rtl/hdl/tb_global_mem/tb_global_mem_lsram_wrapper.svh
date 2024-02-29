5dc733c78d7f8bca13569295d27f7ee0b01e9d6c
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2024-02-29 10:29:26 -0500

    RTL/GlobalMem Run testcase code in separate class file

--- rtl/hdl/tb_global_mem/tb_global_mem_lsram_wrapper.svh
+++ rtl/hdl/tb_global_mem/tb_global_mem_lsram_wrapper.svh
@@ -1,6 +1,40 @@
 class tb_global_mem_lsram_wrapper;
 
-static task run_task();
+static task automatic run_task(
+  // port A input
+  time             ACLK_PER,
+  ref logic        A_CLK,
+  ref logic [10:0] A_ADDR,
+  ref logic [17:0] A_DIN,
+  ref logic [ 1:0] A_WEN,
+  ref logic        A_REN,
+
+  // port A output wires
+  ref logic  [17:0] A_DOUT,
+
+  // port B input
+  time             BCLK_PER,
+  ref logic        B_CLK,
+  ref logic [10:0] B_ADDR,
+  ref logic [17:0] B_DIN,
+  ref logic [ 1:0] B_WEN,
+  ref logic        B_REN,
+
+  // port B output wires
+  ref logic  [17:0] B_DOUT
+  );
+
+  A_ADDR = '0;
+  A_DIN  = 18'h0BEEF;
+  A_WEN  = 2'b11;
+  #(ACLK_PER);
+  A_WEN  = 2'b00;
+
+  #(5*ACLK_PER);
+  A_REN  = 1'b1;
+  #(ACLK_PER);
+  A_REN  = 1'b0;
+  #(10*ACLK_PER);
 
 endtask
 
--- rtl/hdl/tb_global_mem/tb_global_mem_pkg.sv
+++ rtl/hdl/tb_global_mem/tb_global_mem_pkg.sv
@@ -1,8 +1,9 @@
 package tb_global_mem_pkg;
 
-    import uvm_pkg::*;
+  import uvm_pkg::*;
 
-    `include "uvm_macros.svh";
-    `include "tb_global_mem_lsram_wrapper.svh";
+  `include "uvm_macros.svh";
+  `include "../tb_common/mat_conv.svh";
+  `include "tb_global_mem_lsram_wrapper.svh";
 
 endpackage
